7c9dcfe0ae8dff477ac587e146ded1b254d58d78
Alexander Obregon <alexander.obregon47@gmail.com>
2022-02-15 09:16:20 -0800

    Created seed template

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,29 @@
+User.destroy_all
+Post.destroy_all
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+User.create(user_name:"")
+User.create(user_name:"")
+User.create(user_name:"")
+User.create(user_name:"")
+User.create(user_name:"")
+User.create(user_name:"")
+
+
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+Post.create(comment_id: , user_id: , photo_id: , location: , artist: , post_date: , likes: )
+
+# comment_id could be a optional table, we can leave null for now
+
 
 puts "✅ Done seeding!"
