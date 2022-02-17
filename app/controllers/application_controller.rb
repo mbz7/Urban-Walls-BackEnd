89aa7180bad95c2e673c0e7f1b47ea3bb001be60
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-17 12:48:10 -0800

    fixed user_id and delete req for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,56 +1,50 @@
-    class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
 
   # get requests
-  get "/post" do
+  get '/post' do
     # get all post
     all_post = Post.all.order(:id).reverse
     all_post.to_json(include: :user)
   end
 
-  get "/post/:id" do
+  get '/post/:id' do
     # get target post
     target_post = Post.find(params[:id])
     target_post.to_json
   end
 
   # post requests
-    post "/post" do
-    post = Post.create(
-      photo_url: params[:photo_url],
-      location: params[:location],
-      artist: params[:artist],
-      post_date: params[:post_date],
-      likes:0,
-      user_id: params[:user_id],
-      comment_id: params[:comment_id])
-      post.to_json
+  post '/post' do
+   user = User.create(user_name: params[:user_name])
+    post =
+      Post.create(
+        photo_url: params[:photo_url],
+        location: params[:location],
+        artist: params[:artist],
+        post_date: params[:post_date],
+        likes: 0,
+        user_id: User.last.id,)
+   
+    post.to_json
   end
-   post "/user" do
-      user = User.create(
-        user_name: params[:user_name])
-      user.to_json
-    end
+
 
   # Updates
-  patch "/post/:id" do
+  patch '/post/:id' do
     post = Post.find(params[:id])
-    post.update(
-      # photo_url: params[:photo_url],
-      # location: params[:location],
-      # artist: params[:artist],
-      # post_date: params[:post_date],
-      likes: params[:likes])
-      post.to_json(include: :user)
+    post.update(likes: params[:likes])
+    post.to_json(include: :user)
   end
 
   # delete requests
-  delete "/post/:id" do
+  delete '/post/:id' do
     post = Post.find(params[:id])
+    user = post.user
+    user.destroy
     post.destroy
-    post.to_json    
+    post.to_json
   end
-
 end
